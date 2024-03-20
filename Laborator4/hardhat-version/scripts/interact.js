bb6e2bc2f24edeed23bf183de9fc0ae532d6ad19
Dupree7 <asd@asd.com>
2024-03-20 02:13:17 -0700

    update requirements

--- Laborator4/hardhat-version/scripts/interact.js
+++ Laborator4/hardhat-version/scripts/interact.js
@@ -2,7 +2,7 @@ require("@nomiclabs/hardhat-ethers");
 const { ethers } = require("hardhat");
 
 async function interact() {
-    [owner] = await ethers.getSigners();
+    [owner, user1] = await ethers.getSigners();
 
     let deployedTokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
     let token = await ethers.getContractAt("MyERC20", deployedTokenAddress)
@@ -13,6 +13,14 @@ async function interact() {
 
     let userBalance = await token.balanceOf(owner.address)
     console.log("user balance: ", userBalance)
+
+    // Transfer funds from owner to user1
+
+    // Check user 1 balance
+
+    // Approve user 1 to spend from owner
+
+    // Call transfer from to actually spend
 }
 
 interact()
